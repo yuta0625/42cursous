2227d89dd46e4fcdfdfe11dbff26e46ff239ce63
yuta0625 <yuyu.octopus625@icloud.com>
2025-11-05 20:46:10 +0900

    42tokyo

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yohya <yohya@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 14:23:25 by yohya             #+#    #+#             */
-/*   Updated: 2025/11/05 18:34:47 by yohya            ###   ########.fr       */
+/*   Updated: 2025/11/05 20:43:40 by yohya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,30 @@
 char	*get_next_line(int fd)
 {
 	static char		*stash;
-	char			buf[BUFFER_SIZE];
+	char			*buf;
 	ssize_t			n;
 	char			*ret;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	n = read(fd, buf, BUFFER_SIZE);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	n = 1;
 	while ((!gnl_strchr(stash)) && n > 0)
 	{
-		stash = gnl_strjoin_free(stash, buf, n);
+		n = read(fd, buf, BUFFER_SIZE);
+		if (n < 0)
+			return (free(buf), NULL);
+		buf[n] = '\0';
+		if (n > 0)
+			stash = gnl_strjoin_free(stash, buf, n);
 		if (!stash)
-			return (NULL);
-		if (!gnl_strchr(stash))
-			n = read(fd, buf, BUFFER_SIZE);
+			return (free(buf), NULL);
 	}
+	free(buf);
+	if (!stash || *stash == '\0')
+		return (free(stash), stash = NULL, NULL);
 	ret = extraction_stash(stash);
 	stash = trim_stash(stash);
 	return (ret);
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yohya <yohya@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:04:53 by yohya             #+#    #+#             */
-/*   Updated: 2025/11/05 16:13:50 by yohya            ###   ########.fr       */
+/*   Updated: 2025/11/05 20:16:53 by yohya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@
 # include <stdlib.h>
 # include <stdio.h>
 
+char	*get_next_line(int fd);
 int		gnl_strchr(const char *s);
 char	*gnl_strjoin_free(char *old, const char *buf, ssize_t n);
 char	*extraction_stash(char *stash);
 char	*trim_stash(char *stash);
+int		find_nl(const char *s);
 
 #endif
