725ccbd1bce766dd84a07a3fff55b9f6c2482144
yuta0625 <yuyu.octopus625@icloud.com>
2025-12-14 18:54:06 +0900

    12/14 update radix_sort.c

--- push_swap/src/radix_sort.c
+++ push_swap/src/radix_sort.c
@@ -6,38 +6,49 @@
 /*   By: yohya <yohya@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:04:53 by yohya             #+#    #+#             */
-/*   Updated: 2025/12/14 18:06:43 by yohya            ###   ########.fr       */
+/*   Updated: 2025/12/14 18:45:25 by yohya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 //基数ソート
+
+static int	get_max_bits(int n)
+{
+	int	bits;
+
+	bits = 0;
+	while ((n >> bits) != 0)
+		bits++;
+	return (bits);
+}
+
 void	radix_sort(t_stack *a, t_stack *b)
 {
 	int	max_bits;
-	int	max_index;
 	int	bit;
 	int	i;
 	int	n;
 
-	max_index = a->size -1;
-	max_bits = 0;
-	while ((max_index >> max_bits) != 0)
-		max_bits++;
+	if (!a || a->size < 2)
+		return ;
+	max_bits = get_max_bits(a->size -1);
 	bit = 0;
-	while (bit++ < max_bits)
+	while (bit < max_bits)
 	{
 		n = a->size;
 		i = 0;
-		while (i++ < n)
+		while (i < n)
 		{
 			if (((a->top->index >> bit) & 1) == 0)
 				pb(a, b);
 			else
 				ra(a);
+			i++;
 		}
 		while (b->size > 0)
 			pa(a, b);
+		bit++;
 	}
 }
